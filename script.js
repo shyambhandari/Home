4485153e9a454428bcca22b7f58c108ba0fc581e
shyambhandari <35090374+shyambhandari@users.noreply.github.com>
2018-01-05 10:34:59 +0545

    Initial Checkpoint

---
+++ script.js
@@ -0,0 +1,29 @@
+/*alert("WELCOME to my site!");
+
+let message;
+message='Hello!';
+message='World'; //value changed
+alert(message);
+
+const COLOR_RED="#f00";
+const COLOR_GREEN="#0f0";
+const COLOR_BLUE="#h780";
+const COLOR_ORANGE="#hdery56";
+
+//when we need to pick a color
+let color=COLOR_ORANGE;
+alert(color); 
+
+
+let value = true;
+alert(typeof value); // boolean
+
+value = String(value); 
+alert(typeof value); // string
+
+alert("6"/"2"); // 3
+
+
+let s = "my" + "string";
+alert(s);
+*/
